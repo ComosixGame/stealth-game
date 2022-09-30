710d02ed702650c524a9138b93f9106e7b1fad61
tiendat3699 <onesteep00@gmail.com>
2022-09-30 17:43:53 +0700

    add prefab enemy

--- Assets/Scripts/Enemy/EnemyDamageable.cs
+++ Assets/Scripts/Enemy/EnemyDamageable.cs
@@ -1,20 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyDamageable : Damageable
 {
+    Rigidbody[] ragdollRigibodies;
     public override void TakeDamge(Vector3 hitPoint, float damage, float force)
     {
         health -= damage;
         if(health <= 0) {
             Destroy(gameObject);
             GameObject deadBody = Instantiate(DestroyedBody, transform.position, transform.rotation);
+            ragdollRigibodies = deadBody.GetComponentsInChildren<Rigidbody>();
             Vector3 dirForce = transform.position - hitPoint;
             dirForce.y = 0;
             dirForce.Normalize();
-            Rigidbody rb = deadBody.GetComponent<Rigidbody>();
-            rb.AddForceAtPosition(dirForce * force, hitPoint, ForceMode.VelocityChange);
+            Rigidbody hitRigi = ragdollRigibodies.OrderBy(rb => Vector3.Distance(rb.position, hitPoint)).First();
+            hitRigi.AddForceAtPosition(dirForce * force, hitPoint, ForceMode.Impulse);
         }
     }
 }
